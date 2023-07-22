a1604fea20813dd6af2920ea477b0c17152e544e
mniudanri <niudanri12@gmail.com>
2023-07-22 16:49:56 +0700

    add sample route

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-auth-paseto
 
 go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/rs/zerolog v1.29.1
+)
--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	router := gin.Default()
+
+	router.GET("/users", sampleProcess)
+	
+	err := router.Run("0.0.0.0:8080")
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot start server")
+	}
+}
+
+func sampleProcess(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"message": "sample message!"})
+	return
 }
