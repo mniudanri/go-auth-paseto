adf14f847c5536dc06b5289d9d49ad252bb6dc18
mniudanri <niudanri12@gmail.com>
2023-07-31 19:42:24 +0700

    generate new changes psql CRUD

--- db/migration/000001_db_schema.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "users" (
-  "username" varchar PRIMARY KEY,
-  "hashed_password" varchar NOT NULL,
-  "full_name" varchar NOT NULL,
-  "email" varchar UNIQUE NOT NULL,
-  "is_email_verified" bool NOT NULL DEFAULT false,
-  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01',
-  "created_at" timestamptz NOT NULL DEFAULT (now())
-);
