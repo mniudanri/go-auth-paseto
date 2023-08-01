03a99be2c1cb4dec1aeb12ae6ba113f7f8245df4
mniudanri <niudanri12@gmail.com>
2023-08-01 17:33:33 +0700

    add test for user CRUD

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/mniudanri/go-auth-paseto/util"
+)
+
+var testStore Store
+
+func TestMain(m *testing.M) {
+	config := util.LoadConfig("../..")
+
+	connPool, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testStore = NewStore(connPool)
+	os.Exit(m.Run())
+}
